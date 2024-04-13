ef90e8a86b346578a7b9c6177f2500bd809ee830
Jose Manuel Rodriguez Calvo <jmr@MacBook-Pro-de-Jose.local>
2024-04-13 10:51:53 +0200

    login route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { login, signup } from './actions'
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function LoginPage() {
+    return (
+        <Typography>Login</Typography>
+    );
+}
---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+'use client';
+import { Lexend } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const lexend = Lexend({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: lexend.style.fontFamily,
+  }
+});
+
+
+export default theme;
